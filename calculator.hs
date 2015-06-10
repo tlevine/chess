1f755b7eec646749ea6775bded91c8e3412de157
Thomas Levine <_@thomaslevine.com>
2015-06-10 21:54:28 +0000

    j

--- calculator.hs
+++ calculator.hs
@@ -9,6 +9,20 @@ data Token = Number Integer |
              R |
              Equal
 
+-- left, right, operation, stored
+type State = (Integer, Integer, Function, Integer)
+
+updateState :: Token -> State -> State
+updateState (Number n) (left, _, op, stored) -> (left, n, op, stored)
+updateState (Operation f) (left, right, op, stored) -> (op left right, 0, f, stored)
+updateState M (left, right, op, _) -> (left, right, op, right)
+updateState R (left, _, op, stored) -> (left, stored, op, stored)
+updateState Equal (left, right, op, stored) -> (0, op left right, (+), stored)
+
+
+initialState :: State
+initialState = (0, 0, (+), 0) 
+
 -- The left of the calculator program is the beginning of the list;
 -- everything is reversed
 --calculate :: [Token] -> Integer
