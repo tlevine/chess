497c6fa1109ccf0f4903fd7f9e2ce65b998cef51
Thomas Levine <_@thomaslevine.com>
2015-06-10 20:43:20 +0000

    j

--- calculator.hs
+++ calculator.hs
@@ -1,4 +1,28 @@
-data StorageOperation = M | R
+type Operation = Integer -> Integer -> Integer
+
+-- Current number, stored number, current operation
+type State = (Integer, Integer, Operation)
+
+-- Each character is a command.
+type Command = State -> State
+
+emptyOperation :: Operation
+emptyOperation left right = (left * 10) + right
+
+m :: Command
+m (current, _) = (current, current)
+
+r :: Command
+r :: (_, stored) = (stored, stored)
+
+operate :: Operation -> Command
+operate operation (current, stored, _) = (current, stored, operation)
+
+
+
+
+
+
 
 data Token = Digit Integer |
              Operation (Integer -> Integer) |
